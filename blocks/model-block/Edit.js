eaa0287af013681bfca576dcf4a91ddc17396177
antpb <itartist.pdf@gmail.com>
2022-09-22 00:58:24 -0500

    adds editor styling

--- blocks/model-block/Edit.js
+++ blocks/model-block/Edit.js
@@ -291,6 +291,7 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 				<>
 					{ attributes.threeObjectUrl ? (
 						<div className="three-object-viewer-inner">
+							<div className="three-object-viewer-inner-edit-container">
 							<svg
 								class="custom-icon custom-icon-cube"
 								viewBox="0 0 40 40"
@@ -302,6 +303,7 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 								</g>
 							</svg>
 							<p><b>Model block</b></p>
+							</div>
 							{/* <p>URL: {attributes.threeObjectUrl}</p> */}
 						</div>					
 				) : (
@@ -344,18 +346,20 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 				<>
 					{ attributes.threeObjectUrl ? (
 						<div className="three-object-viewer-inner">
-							<svg
-								class="custom-icon custom-icon-cube"
-								viewBox="0 0 40 40"
-								version="1.1"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<g transform="matrix(1,0,0,1,-1.1686,0.622128)">
-									<path d="M37.485,28.953L21.699,38.067L21.699,19.797L37.485,10.683L37.485,28.953ZM21.218,19.821L21.218,38.065L5.435,28.953L5.435,10.709L21.218,19.821ZM37.207,10.288L21.438,19.392L5.691,10.301L21.46,1.197L37.207,10.288Z" />
-								</g>
-							</svg>
-							<p><b>Model block</b></p>
+							<div className="three-object-viewer-inner-edit-container">
+								<svg
+									class="custom-icon custom-icon-cube"
+									viewBox="0 0 40 40"
+									version="1.1"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g transform="matrix(1,0,0,1,-1.1686,0.622128)">
+										<path d="M37.485,28.953L21.699,38.067L21.699,19.797L37.485,10.683L37.485,28.953ZM21.218,19.821L21.218,38.065L5.435,28.953L5.435,10.709L21.218,19.821ZM37.207,10.288L21.438,19.392L5.691,10.301L21.46,1.197L37.207,10.288Z" />
+									</g>
+								</svg>
+								<p><b>Model block</b></p>
 							{/* <p>URL: {attributes.threeObjectUrl}</p> */}
+							</div>
 						</div>					
 					) : (
 						<div className="glb-preview-container">
