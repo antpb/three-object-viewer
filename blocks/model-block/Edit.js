5e1ad7235f983a904e1377bbf822275136fd6f46
antpb <itartist.pdf@gmail.com>
2022-10-12 21:51:13 -0500

    adds name property to models for ease of editing

--- blocks/model-block/Edit.js
+++ blocks/model-block/Edit.js
@@ -125,7 +125,7 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 						<PanelRow>
 							<TextControl
 								label="Name"
-								help="Give your object a name"
+								help="Give your object a name."
 								value={ attributes.name }
 								onChange={ ( value ) =>
 									onChangeName( value )
@@ -304,21 +304,24 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 			{ isSelected ? (
 				<>
 					{ attributes.threeObjectUrl ? (
-						<div className="three-object-viewer-inner">
-							<div className="three-object-viewer-inner-edit-container">
-							<svg
-								class="custom-icon custom-icon-cube"
-								viewBox="0 0 40 40"
-								version="1.1"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<g transform="matrix(1,0,0,1,-1.1686,0.622128)">
-									<path d="M37.485,28.953L21.699,38.067L21.699,19.797L37.485,10.683L37.485,28.953ZM21.218,19.821L21.218,38.065L5.435,28.953L5.435,10.709L21.218,19.821ZM37.207,10.288L21.438,19.392L5.691,10.301L21.46,1.197L37.207,10.288Z" />
-								</g>
-							</svg>
-							<p><b>Model block</b></p>
+						//Not selected
+						<div class="three-object-viewer-component-container">
+							<div className="three-object-viewer-inner">
+								<div className="three-object-viewer-inner-edit-container">
+								<svg
+									class="custom-icon custom-icon-cube"
+									viewBox="0 0 40 40"
+									version="1.1"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g transform="matrix(1,0,0,1,-1.1686,0.622128)">
+										<path d="M37.485,28.953L21.699,38.067L21.699,19.797L37.485,10.683L37.485,28.953ZM21.218,19.821L21.218,38.065L5.435,28.953L5.435,10.709L21.218,19.821ZM37.207,10.288L21.438,19.392L5.691,10.301L21.46,1.197L37.207,10.288Z" />
+									</g>
+								</svg>
+								<p><b>Model block</b></p>
+								</div>
+								<p class="three-object-viewer-model-name">{attributes.name}</p>
 							</div>
-							{/* <p>URL: {attributes.threeObjectUrl}</p> */}
 						</div>					
 				) : (
 					<div className="three-object-viewer-inner">
@@ -355,22 +358,24 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 			) : (
 				<>
 					{ attributes.threeObjectUrl ? (
-						<div className="three-object-viewer-inner">
-							<div className="three-object-viewer-inner-edit-container">
-								<svg
-									class="custom-icon custom-icon-cube"
-									viewBox="0 0 40 40"
-									version="1.1"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<g transform="matrix(1,0,0,1,-1.1686,0.622128)">
-										<path d="M37.485,28.953L21.699,38.067L21.699,19.797L37.485,10.683L37.485,28.953ZM21.218,19.821L21.218,38.065L5.435,28.953L5.435,10.709L21.218,19.821ZM37.207,10.288L21.438,19.392L5.691,10.301L21.46,1.197L37.207,10.288Z" />
-									</g>
-								</svg>
-								<p><b>Model block</b></p>
-							{/* <p>URL: {attributes.threeObjectUrl}</p> */}
+						<div class="three-object-viewer-component-container">
+							<div className="three-object-viewer-inner">
+								<div className="three-object-viewer-inner-edit-container">
+									<svg
+										class="custom-icon custom-icon-cube"
+										viewBox="0 0 40 40"
+										version="1.1"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<g transform="matrix(1,0,0,1,-1.1686,0.622128)">
+											<path d="M37.485,28.953L21.699,38.067L21.699,19.797L37.485,10.683L37.485,28.953ZM21.218,19.821L21.218,38.065L5.435,28.953L5.435,10.709L21.218,19.821ZM37.207,10.288L21.438,19.392L5.691,10.301L21.46,1.197L37.207,10.288Z" />
+										</g>
+									</svg>
+									<p><b>Model block</b></p>
+								</div>
+								<p class="three-object-viewer-model-name">{attributes.name}</p>
 							</div>
-						</div>					
+						</div>
 					) : (
 						<div className="three-object-viewer-inner">
 						<div className="three-object-viewer-inner-edit-container">
