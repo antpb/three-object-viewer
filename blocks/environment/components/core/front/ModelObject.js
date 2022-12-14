006466b2f8d69247e4660360279a89b0e6551634
antpb <itartist.pdf@gmail.com>
2022-12-14 00:25:38 -0600

    add editor changes

--- blocks/environment/components/core/front/ModelObject.js
+++ blocks/environment/components/core/front/ModelObject.js
@@ -1,171 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useLoader, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { AudioListener, Quaternion, VectorKeyframeTrack, QuaternionKeyframeTrack, LoopPingPong, AnimationClip, NumberKeyframeTrack, AnimationMixer, Vector3, BufferGeometry, MeshBasicMaterial, DoubleSide, Mesh, CircleGeometry, sRGBEncoding } from "three";
+import { AudioListener, Vector3, BufferGeometry, MeshBasicMaterial, DoubleSide, Mesh, CircleGeometry, sRGBEncoding } from "three";
 import { RigidBody } from "@react-three/rapier";
 import {
 	useAnimations,
 } from "@react-three/drei";
 import { GLTFAudioEmitterExtension } from "three-omi";
 import { GLTFGoogleTiltBrushMaterialExtension } from "three-icosa";
-import { VRMUtils, VRMLoaderPlugin, VRMExpressionPresetName } from "@pixiv/three-vrm";
-
-/**
- * A map from Mixamo rig name to VRM Humanoid bone name
- */
-const mixamoVRMRigMap = {
-	mixamorigHips: 'hips',
-	mixamorigSpine: 'spine',
-	mixamorigSpine1: 'chest',
-	mixamorigSpine2: 'upperChest',
-	mixamorigNeck: 'neck',
-	mixamorigHead: 'head',
-	mixamorigLeftShoulder: 'leftShoulder',
-	mixamorigLeftArm: 'leftUpperArm',
-	mixamorigLeftForeArm: 'leftLowerArm',
-	mixamorigLeftHand: 'leftHand',
-	mixamorigLeftHandThumb1: 'leftThumbMetacarpal',
-	mixamorigLeftHandThumb2: 'leftThumbProximal',
-	mixamorigLeftHandThumb3: 'leftThumbDistal',
-	mixamorigLeftHandIndex1: 'leftIndexProximal',
-	mixamorigLeftHandIndex2: 'leftIndexIntermediate',
-	mixamorigLeftHandIndex3: 'leftIndexDistal',
-	mixamorigLeftHandMiddle1: 'leftMiddleProximal',
-	mixamorigLeftHandMiddle2: 'leftMiddleIntermediate',
-	mixamorigLeftHandMiddle3: 'leftMiddleDistal',
-	mixamorigLeftHandRing1: 'leftRingProximal',
-	mixamorigLeftHandRing2: 'leftRingIntermediate',
-	mixamorigLeftHandRing3: 'leftRingDistal',
-	mixamorigLeftHandPinky1: 'leftLittleProximal',
-	mixamorigLeftHandPinky2: 'leftLittleIntermediate',
-	mixamorigLeftHandPinky3: 'leftLittleDistal',
-	mixamorigRightShoulder: 'rightShoulder',
-	mixamorigRightArm: 'rightUpperArm',
-	mixamorigRightForeArm: 'rightLowerArm',
-	mixamorigRightHand: 'rightHand',
-	mixamorigRightHandPinky1: 'rightLittleProximal',
-	mixamorigRightHandPinky2: 'rightLittleIntermediate',
-	mixamorigRightHandPinky3: 'rightLittleDistal',
-	mixamorigRightHandRing1: 'rightRingProximal',
-	mixamorigRightHandRing2: 'rightRingIntermediate',
-	mixamorigRightHandRing3: 'rightRingDistal',
-	mixamorigRightHandMiddle1: 'rightMiddleProximal',
-	mixamorigRightHandMiddle2: 'rightMiddleIntermediate',
-	mixamorigRightHandMiddle3: 'rightMiddleDistal',
-	mixamorigRightHandIndex1: 'rightIndexProximal',
-	mixamorigRightHandIndex2: 'rightIndexIntermediate',
-	mixamorigRightHandIndex3: 'rightIndexDistal',
-	mixamorigRightHandThumb1: 'rightThumbMetacarpal',
-	mixamorigRightHandThumb2: 'rightThumbProximal',
-	mixamorigRightHandThumb3: 'rightThumbDistal',
-	mixamorigLeftUpLeg: 'leftUpperLeg',
-	mixamorigLeftLeg: 'leftLowerLeg',
-	mixamorigLeftFoot: 'leftFoot',
-	mixamorigLeftToeBase: 'leftToes',
-	mixamorigRightUpLeg: 'rightUpperLeg',
-	mixamorigRightLeg: 'rightLowerLeg',
-	mixamorigRightFoot: 'rightFoot',
-	mixamorigRightToeBase: 'rightToes',
-};
-
-/* global THREE, mixamoVRMRigMap */
-
-/**
- * Load Mixamo animation, convert for three-vrm use, and return it.
- *
- * @param {string} url A url of mixamo animation data
- * @param {VRM} vrm A target VRM
- * @returns {Promise<THREE.AnimationClip>} The converted AnimationClip
- */
-function loadMixamoAnimation( url, vrm ) {
-
-	const loader = new FBXLoader(); // A loader which loads FBX
-	return loader.loadAsync( url ).then( ( asset ) => {
-
-		const clip = AnimationClip.findByName( asset.animations, 'mixamo.com' ); // extract the AnimationClip
-
-		const tracks = []; // KeyframeTracks compatible with VRM will be added here
-
-		const restRotationInverse = new Quaternion();
-		const parentRestWorldRotation = new Quaternion();
-		const _quatA = new Quaternion();
-		const _vec3 = new Vector3();
-
-		// Adjust with reference to hips height.
-		const motionHipsHeight = asset.getObjectByName( 'mixamorigHips' ).position.y;
-		const vrmHipsY = vrm.humanoid?.getNormalizedBoneNode( 'hips' ).getWorldPosition( _vec3 ).y;
-		const vrmRootY = vrm.scene.getWorldPosition( _vec3 ).y;
-		const vrmHipsHeight = Math.abs( vrmHipsY - vrmRootY );
-		const hipsPositionScale = vrmHipsHeight / motionHipsHeight;
-
-		clip.tracks.forEach( ( track ) => {
-			console.log("track: ", track)
-
-			// Convert each tracks for VRM use, and push to `tracks`
-			const trackSplitted = track.name.split( '.' );
-			const mixamoRigName = trackSplitted[ 0 ];
-			const vrmBoneName = mixamoVRMRigMap[ mixamoRigName ];
-			const vrmNodeName = vrm.humanoid?.getNormalizedBoneNode( vrmBoneName )?.name;
-			const mixamoRigNode = asset.getObjectByName( mixamoRigName );
-
-			if ( vrmNodeName != null ) {
-
-				const propertyName = trackSplitted[ 1 ];
-
-				// Store rotations of rest-pose.
-				mixamoRigNode.getWorldQuaternion( restRotationInverse ).invert();
-				mixamoRigNode.parent.getWorldQuaternion( parentRestWorldRotation );
-
-				if ( track instanceof QuaternionKeyframeTrack ) {
-
-					// Retarget rotation of mixamoRig to NormalizedBone.
-					for ( let i = 0; i < track.values.length; i += 4 ) {
-
-						const flatQuaternion = track.values.slice( i, i + 4 );
-
-						_quatA.fromArray( flatQuaternion );
-
-						// 親のレスト時ワールド回転 * トラックの回転 * レスト時ワールド回転の逆
-						_quatA
-							.premultiply( parentRestWorldRotation )
-							.multiply( restRotationInverse );
-
-						_quatA.toArray( flatQuaternion );
-
-						flatQuaternion.forEach( ( v, index ) => {
-
-							track.values[ index + i ] = v;
-
-						} );
-
-					}
-
-					tracks.push(
-						new QuaternionKeyframeTrack(
-							`${vrmNodeName}.${propertyName}`,
-							track.times,
-							track.values.map( ( v, i ) => ( vrm.meta?.metaVersion === '0' && i % 2 === 0 ? - v : v ) ),
-						),
-					);
-
-				} else if ( track instanceof VectorKeyframeTrack ) {
-
-					const value = track.values.map( ( v, i ) => ( vrm.meta?.metaVersion === '0' && i % 3 !== 1 ? - v : v ) * hipsPositionScale );
-					tracks.push( new VectorKeyframeTrack( `${vrmNodeName}.${propertyName}`, track.times, value ) );
-
-				}
-
-			}
-
-		} );
-
-		return new AnimationClip( 'vrmAnimation', clip.duration, tracks );
-
-	} );
-
-}
+import { VRMUtils, VRMLoaderPlugin } from "@pixiv/three-vrm";
 
 /**
  * Represents a model object in a virtual reality scene.
@@ -175,14 +19,13 @@ function loadMixamoAnimation( url, vrm ) {
  * @return {JSX.Element} The model object.
  */
 export function ModelObject(model) {
-	const idleFile = model.threeObjectPlugin + model.idle;
 	const [clicked, setClickEvent] = useState();
 	const [url, set] = useState(model.url);
 	useEffect(() => {
 		setTimeout(() => set(model.url), 2000);
 	}, []);
 	const [listener] = useState(() => new AudioListener());
-	const { scene, clock } = useThree();
+
 	useThree(({ camera }) => {
 		camera.add(listener);
 	});
@@ -207,10 +50,9 @@ export function ModelObject(model) {
 			);
 		}
 		loader.register((parser) => {
-			return new VRMLoaderPlugin(parser, {autoUpdateHumanBones: true});
+			return new VRMLoaderPlugin(parser);
 		});
 	});
-	
 
 	const audioObject = gltf.scene.getObjectByProperty('type', 'Audio');
 
@@ -243,48 +85,19 @@ export function ModelObject(model) {
 
 	if (gltf?.userData?.gltfExtensions?.VRM) {
 		const vrm = gltf.userData.vrm;
-	
 		vrm.scene.position.set(
 			model.positionX,
 			model.positionY,
 			model.positionZ
 		);
-
 		VRMUtils.rotateVRM0(vrm);
 		const rotationVRM = vrm.scene.rotation.y + parseFloat(0);
 		vrm.scene.rotation.set(0, rotationVRM, 0);
 		vrm.scene.scale.set(1, 1, 1);
 		vrm.scene.scale.set(model.scaleX, model.scaleY, model.scaleZ);
-
-		vrm.scene.name = "assistant";
-		// scene.add(vrm.scene);
-		console.log(vrm);
-		useEffect(() => {
-			if(vrm){
-				let currentMixer = new AnimationMixer( vrm.scene );
-				loadMixamoAnimation( idleFile, vrm ).then( ( clip ) => {
-					// console.log("clip", clip);
-					// Apply the loaded animation to mixer and play
-					console.log(currentMixer.clipAction( clip ).play());
-				} );
-			}
-		}, [vrm]);
-
-
-		const { actions } = useAnimations(vrm.scene.animations, vrm.scene);
-		useEffect(() => {
-			if (animationList) {
-				actions.forEach((name) => {
-						console.log(actions[name].play());
-				});
-			}
-		}, []);
-		scene.add(vrm.scene);
-		// vrm.scene.animations[0].play();
 		return (
-			<></>
 			// <A11y role="content" description={model.alt} showAltText >
-			// <primitive object={vrm.scene} />
+			<primitive object={vrm.scene} />
 			// </A11y>
 		);
 	}
