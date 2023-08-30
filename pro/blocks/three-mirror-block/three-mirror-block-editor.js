c71cfc5a1c6454906cdc2ef99308a56bab0cca93
antpb <itartist.pdf@gmail.com>
2023-08-29 19:42:04 -0500

    adds transform controls to edit component

--- pro/blocks/three-mirror-block/three-mirror-block-editor.js
+++ pro/blocks/three-mirror-block/three-mirror-block-editor.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import { Reflector } from 'three/examples/jsm/objects/Reflector';
+import {
+	TransformControls,
+} from "@react-three/drei";
 
 import {
   PlaneGeometry,
@@ -8,6 +11,14 @@ import {
 } from "three";
 
 export function ThreeMirror(threeMirror) {
+	const mirrorObj = useRef();
+	const [isSelected, setIsSelected] = useState();
+	const mirrorBlockAttributes = wp.data
+		.select("core/block-editor")
+		.getBlockAttributes(text.htmlobjectId);
+	const TransformController = ({ condition, wrap, children }) =>
+		condition ? wrap(children) : children;
+
   const { scene } = useThree();
 
   useEffect(() => {
@@ -29,7 +40,68 @@ export function ThreeMirror(threeMirror) {
     mirror.rotation.set(threeMirror.rotationX, threeMirror.rotationY, threeMirror.rotationZ);
     // add the mirror to the scene
     scene.add(mirror);
-  }, []);
+  }, [threeMirror.positionX, threeMirror.positionY, threeMirror.positionZ, threeMirror.rotationX, threeMirror.rotationY, threeMirror.rotationZ, threeMirror.scaleX, threeMirror.scaleY, threeMirror.scaleZ]);
 
-  return;
+	return (
+		<Select
+			box
+			multiple
+			onChange={(e) => {
+				e.length !== 0 ? setIsSelected(true) : setIsSelected(false);
+			}}
+			filter={(items) => items}
+		>
+			<TransformController
+				condition={threeMirror.focusID === threeMirror.htmlobjectId}
+				wrap={(children) => (
+					<TransformControls
+						mode={threeMirror.transformMode}
+						enabled={threeMirror.focusID === threeMirror.htmlobjectId}
+						object={mirrorObj}
+						size={0.5}
+						onObjectChange={(e) => {
+							const rot = new THREE.Euler(0, 0, 0, "XYZ");
+							const scale = e?.target.worldScale;
+							rot.setFromQuaternion(
+								e?.target.worldQuaternion
+							);
+							wp.data
+								.dispatch("core/block-editor")
+								.updateBlockAttributes(text.htmlobjectId, {
+									positionX: e?.target.worldPosition.x,
+									positionY: e?.target.worldPosition.y,
+									positionZ: e?.target.worldPosition.z,
+									rotationX: rot.x,
+									rotationY: rot.y,
+									rotationZ: rot.z,
+									scaleX: scale.x,
+									scaleY: scale.y,
+									scaleZ: scale.z
+								});
+						}}
+					>
+						{children}
+					</TransformControls>
+				)}
+			>
+				{mirrorBlockAttributes && (
+					<group
+						ref={mirrorObj}
+						position={[
+							mirrorBlockAttributes.positionX,
+							mirrorBlockAttributes.positionY,
+							mirrorBlockAttributes.positionZ
+						]}
+						rotation={[
+							mirrorBlockAttributes.rotationX,
+							mirrorBlockAttributes.rotationY,
+							mirrorBlockAttributes.rotationZ
+						]}
+						scale={[mirrorBlockAttributes.scaleX, mirrorBlockAttributes.scaleY, mirrorBlockAttributes.scaleZ]}
+					>
+					</group>
+				)}
+			</TransformController>
+		</Select>
+	);
 }
